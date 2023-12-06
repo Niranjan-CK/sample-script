ff99d0aeaed29769f64074d38da4f96c9115b4ed
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2023-12-06 11:31:30 +0530

    Update thank_You.js

--- thank_You.js
+++ thank_You.js
@@ -1,14 +1,14 @@
 // Wait for the DOM to be ready
 document.addEventListener('DOMContentLoaded', function () {
-  
+  console.log("start")
   // Find the element with the class .order-summary__section--total-lines
   var totalLinesSection = document.querySelector('.order-summary__section--total-lines');
-
+  console.log(totalLinesSection)
   // Check if the element is found
   if (totalLinesSection) {
     // Create a new div element
     var newDiv = document.createElement('div');
-
+    console.log("enter");
     // Set the content or attributes for the new div if needed
     newDiv.textContent = 'Your content goes here'; // Replace with your content
 
