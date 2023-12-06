50a890f8610028472539830786f7fc9ed38fa54c
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2023-12-06 09:26:26 +0530

    Update and rename sample.js to thankYou.js

---
+++ thankYou.js
@@ -0,0 +1,17 @@
+// Wait for the DOM to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Find the element with the class .order-summary__section--total-lines
+  var totalLinesSection = document.querySelector('.order-summary__section--total-lines');
+
+  // Check if the element is found
+  if (totalLinesSection) {
+    // Create a new div element
+    var newDiv = document.createElement('div');
+
+    // Set the content or attributes for the new div if needed
+    newDiv.textContent = 'Your content goes here'; // Replace with your content
+
+    // Append the new div under the .order-summary__section--total-lines element
+    totalLinesSection.appendChild(newDiv);
+  }
+});
