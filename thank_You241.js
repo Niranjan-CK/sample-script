a354bf0626385148e5ed593038e6969a61c16223
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2023-12-07 15:51:31 +0530

    Update and rename thank_You241.js to thank_You541.js

--- thank_You241.js
+++
@@ -1,4 +0,0 @@
-console.log("test 1");
-window.onload =  function() {
-console.log("test 1");
-}
---
+++ thank_You541.js
@@ -0,0 +1,6 @@
+console.log("test 1");
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("test 2");
+  // Your additional code here
+});
