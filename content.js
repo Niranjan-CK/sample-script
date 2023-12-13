7e729f5a48df5e99ca279d1a996832ba9bf90551
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2023-12-13 11:23:37 +0530

    Create content.js

---
+++ content.js
@@ -0,0 +1,40 @@
+{
+  "en": {
+    "widget": {
+      "elements": {
+        "buttons": {
+          "elements": {
+            "cart": "Add to cart",
+            "out_of_stock": "Out of stock"
+          }
+        },
+        "footer": {
+          "poweredby": {
+            "link": "https://www.storefrog.com",
+            "title": "Powered by storefrog"
+          }
+        },
+        "title": "Best sellers in store"
+      }
+    }
+  },
+  "fr": {
+    "widget": {
+      "elements": {
+        "buttons": {
+          "elements": {
+            "cart": "Add to cart",
+            "out_of_stock": "Out of stock"
+          }
+        },
+        "footer": {
+          "poweredby": {
+            "link": "https://www.storefrog.com",
+            "title": "Powered by storefrog"
+          }
+        },
+        "title": "Best sellers in store"
+      }
+    }
+  }
+}
